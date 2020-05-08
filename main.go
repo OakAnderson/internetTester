5d09be62b65d5e428091f9b7f758791d20a7e14d
OakAnderson <andersonfelipe01@live.com>
2020-05-08 09:53:41 -0300

    New example to use

--- main.go
+++ main.go
@@ -1,27 +1,14 @@
 package main
 
 import (
-    "os"
-    "time"
-    "strconv"
+	"time"
+
+	"github.com/OakAnderson/internetTester/nettest"
 )
 
 func main() {
-    interval := 5.0
-    if len(os.Args) > 1 {
-        var err error
-        interval, err = strconv.ParseFloat(os.Args[1], 64)
-        if err != nil {
-            interval = 5.0
-        }
-    }
-
-    FillDataOnStructs()
-    count := time.Now()
-    for {
-        if float64(time.Since(count).Minutes()) > interval {
-            FillDataOnStructs()
-            count = time.Now()
-        }
-    }
+	err := nettest.MultiTests(-1, true, true, time.Minute*10)
+	if err != nil {
+		panic(err)
+	}
 }
