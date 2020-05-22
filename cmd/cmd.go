adf5d28208a4f4856c68fd91f7e90270d28c6939
OakAnderson <andersonfelipe01@live.com>
2020-05-22 00:14:48 -0300

    Corrected cmd bug interval

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -21,19 +21,25 @@ func GetIntervals(intervals []string) ([]time.Duration, error) {
 		"h": time.Hour,
 		"m": time.Minute,
 		"s": time.Second,
-		"":  time.Duration(1),
 	}
 
 	duration, _ := regexp.Compile("^([0-9])*([msh])?$")
 	var timeIntervals []time.Duration
 	for _, t := range intervals {
-		args := duration.FindStringSubmatch(t)
-		if args == nil {
+		if !duration.MatchString(t) {
 			return nil, fmt.Errorf("usage of intervals: 10 (seconds), 10m (minutes), 10s (seconds), 10h (hours)")
 		}
 
-		num, _ := strconv.Atoi(args[1])
-		timeIntervals = append(timeIntervals, metrics[args[2]]*time.Duration(num))
+		tm, ok := metrics[string(t[len(t)-1])]
+		var num int
+		if !ok {
+			tm = time.Second
+			num, _ = strconv.Atoi(t)
+		} else {
+			num, _ = strconv.Atoi(t[:len(t)-1])
+		}
+
+		timeIntervals = append(timeIntervals, tm*time.Duration(num))
 	}
 
 	return timeIntervals, nil
